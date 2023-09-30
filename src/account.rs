e083f94c45d652e70b2b5f9c86eb98b7c8120dca
yaonam <elimviolinist@gmail.com>
2023-09-30 09:48:25 -0400

    got signature working

--- src/account.rs
+++ src/account.rs
@@ -1,4 +1,13 @@
+use base64::{
+    alphabet,
+    engine::{self, general_purpose},
+    Engine as _,
+};
+use hex;
+use hmac::{Hmac, Mac};
+use sha2::{Digest, Sha256, Sha512};
 use std::collections::HashMap;
+use std::str;
 
 pub struct Portfolio {
     assets: HashMap<String, (f64, f64)>, // (amount, price)
@@ -18,14 +27,14 @@ impl Portfolio {
         }
     }
 
-    // Returns how many asset units the portfolio is away from the target.
+    // value/total - target. In units of base asset (USD).
     pub fn get_asset_target_delta(&self, asset: String) -> f64 {
         let (amount, price) = self.get_asset(asset);
         let total_value = self.get_total_value();
 
-        let target = total_value / self.assets.len() as f64 / price;
+        let target = 1.0 / self.assets.len() as f64;
 
-        amount - target
+        amount * price / total_value - target
     }
 
     fn get_asset_allocation(&self, asset: String) -> f64 {
@@ -43,5 +52,42 @@ impl Portfolio {
 
     pub fn set_asset(&mut self, asset: String, amount: f64, price: f64) {
         self.assets.insert(asset, (amount, price));
+    }
+}
+
+pub struct Signer {
+    key: String,
+    secret: String,
+}
+
+impl Signer {
+    pub fn new(key: String, secret: String) -> Self {
+        Signer {
+            key: key,
+            secret: secret,
+        }
+    }
+
+    pub fn sign(&self, url: String, data: String) -> String {
+        let mut hasher = Sha256::new();
+        let payload =
+            b"nonce=1616492376594&ordertype=limit&pair=XBTUSD&price=37500&type=buy&volume=1.25";
+        hasher.update(b"1616492376594");
+        hasher.update(payload);
+        let encoded_payload = &hasher.finalize();
+
+        println!("encoded_payload: {:?}", encoded_payload);
+
+        let key_base64 = "kQH5HW/8p1uGOVjbgWA7FunAmGO8lsSUXNsu3eow76sz84Q18fWxnyRzBHCd3pd5nE9qa99HAZtuZuj6F1huXg==";
+        let key_vec = general_purpose::STANDARD.decode(key_base64).unwrap();
+        let key_bytes = key_vec.as_slice();
+        let content = [b"/0/private/AddOrder" as &[u8], encoded_payload].concat();
+        let mut mac = Hmac::<Sha512>::new_from_slice(key_bytes).expect("Couldn't create HMAC");
+        mac.update(&content);
+        let signature = mac.finalize().into_bytes();
+
+        let sign = general_purpose::STANDARD.encode(signature);
+
+        sign
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use rebalancer::account::Portfolio;
+use rebalancer::account::{Portfolio, Signer};
 use rebalancer::task;
 use std::sync::{Arc, Mutex};
 use tokio::signal::ctrl_c;
 
 #[tokio::main]
 async fn main() {
-    let pair1 = "ETH/USD".to_string();
-    let pair2 = "STORJ/USD".to_string();
+    let signer = Signer::new("".to_string(), "".to_string());
+    let result = signer.sign("".to_string(), "".to_string());
+    println!("{}", result);
 
-    let portfolio = Arc::new(Mutex::new(Portfolio::new()));
+    // let pair1 = "ETH/USD".to_string();
+    // let pair2 = "STORJ/USD".to_string();
 
-    let task1 = tokio::spawn(task::start(pair1, portfolio.clone()));
-    let task2 = tokio::spawn(task::start(pair2, portfolio.clone()));
+    // let portfolio = Arc::new(Mutex::new(Portfolio::new()));
 
-    let _ = tokio::select! {
-        _ = task1 => (),
-        _ = task2 => (),
-        _ = ctrl_c() => (), // Graceful shutdown
-    };
+    // let task1 = tokio::spawn(task::start(pair1, portfolio.clone()));
+    // let task2 = tokio::spawn(task::start(pair2, portfolio.clone()));
+
+    // let _ = tokio::select! {
+    //     _ = task1 => (),
+    //     _ = task2 => (),
+    //     _ = ctrl_c() => (), // Graceful shutdown
+    // };
 }
